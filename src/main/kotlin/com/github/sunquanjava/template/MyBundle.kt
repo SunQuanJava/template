8ff4c37b7e2058490bdd5650672305e100920bae
GitHub Action <action@github.com>
2021-06-09 11:42:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/sunquanjava/template/MyBundle.kt
+++ src/main/kotlin/com/github/sunquanjava/template/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sunquanjava.template
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sunquanjava/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sunquanjava/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sunquanjava.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sunquanjava.template.MyBundle
 
 class MyApplicationService {
 
